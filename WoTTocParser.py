8ae537f55a20b8bcbe9c7ddaf234c1de2160cd97
RajivMotePro <rajivmotepro@gmail.com>
2018-03-02 17:23:21 -0600

    Fix encoding issue

--- WoTTocParser.py
+++ WoTTocParser.py
@@ -9,35 +9,37 @@ class WoTTocParser:
         with open(toc_file_name, "r") as toc_file:
             root = xml.etree.ElementTree.parse(toc_file).getroot()
             navMap = root[3]
-            for book in navMap:
-                bookName = book[0][0].text
-                if bookName.isupper():
-                    print(bookName)
-                    for chapter in book.findall("{http://www.daisy.org/z3986/2005/ncx/}navPoint"):
-                        chapterName= chapter[0][0].text
-                        if (chapterName.startswith("Prologue")
-                                | chapterName.startswith("Epilogue")
-                                | chapterName[:1].isdigit()):
-                            source = chapter[1].get("src")
-                            source_file_name = os.path.join(base_dir_name, source)
-                            print(chapterName + '\t' + source_file_name)
-                            with open(source_file_name, "r") as source_file:
-                                stripper = MLStripper()
-                                chapter_text = source_file.read()
-                                stripper.feed(chapter_text)
-                                chapter_text = stripper.get_data()
-                                print(chapter_text)
+            wot_text_name = os.path.join(base_dir_name, "wot_all.txt")
+            with open(wot_text_name, "w", encoding="utf8") as wot_text_file:
+                for book in navMap:
+                    bookName = book[0][0].text
+                    if bookName.isupper():
+                        print(bookName)
+                        for chapter in book.findall("{http://www.daisy.org/z3986/2005/ncx/}navPoint"):
+                            chapterName= chapter[0][0].text
+                            if (chapterName.startswith("Prologue")
+                                    | chapterName.startswith("Epilogue")
+                                    | chapterName[:1].isdigit()):
+                                source = chapter[1].get("src")
+                                source_file_name = os.path.join(base_dir_name, source)
+                                print(chapterName + '\t' + source_file_name)
+                                with open(source_file_name, "r", encoding="utf8") as source_file:
+                                    stripper = MLStripper()
+                                    chapter_text = source_file.read()
+                                    stripper.feed(chapter_text)
+                                    chapter_text = stripper.get_data()
+                                    wot_text_file.write(chapter_text)
 
 def main():
     if len(sys.argv) > 1:
         base_dir_name = sys.argv[1]
+        parser = WoTTocParser()
         try:
             print("Converting: " + base_dir_name)
-            parser = WoTTocParser()
             parser.parse(base_dir_name)
             print("Converted: " + base_dir_name)
-        except BaseException:
-            print("Caught an exception: " + BaseException.__str__())
+        except BaseException as ex:
+            print(ex)
     else:
         print("No directory specified")
 
